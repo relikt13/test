e517fd1fd8780dc08dd9ca97b517d19d7c73a3d3
Illya Tishkevich <you@example.com>
2018-11-13 00:50:04 +0300

    1.9 homework

---
+++ Training/Test/Plugin/Controller/Products/View.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Training\Test\Plugin\Controller\Products;
+
+class View {
+    protected $customerSession;
+    protected $redirectFactory;
+    
+    public function __construct(
+                                \Magento\Customer\Model\Session $customerSession,
+                                \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
+                                 )
+    {
+        $this->customerSession = $customerSession; 
+        $this->redirectFactory = $redirectFactory;
+    }
+          
+          
+    public function aroundExecute(
+                                  \Magento\Catalog\Controller\Product\View $subject,
+                                  \Closure $proceed
+                                )
+    {
+        if (!$this->customerSession->isLoggedIn()) {
+                    return $this->redirectFactory->create()->setPath('customer/account/login');
+        }
+        return $proceed();
+    }
+}
